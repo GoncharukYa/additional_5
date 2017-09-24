d3e83c7f501e2edc8e2cc768ef752e0d3c8842c3
Goncharuk Yaroslav <gonyarik@gmail.com>
2017-09-24 16:49:01 +0300

    Del console.log(), add commets

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ module.exports = function check(str, bracketsConfig) {
   let closeBrackets = [];
   let sameBrackets = [];
 
+// Collecting openBrackets, closeBrackets and sameBrackets.
   for (let i = 0; i < bracketsConfig.length; i++) {
     openBrackets.push(bracketsConfig[i][0]);
     closeBrackets.push(bracketsConfig[i][1]);
@@ -14,56 +15,36 @@ module.exports = function check(str, bracketsConfig) {
     }
   }
 
-  console.log("openBrackets- " + openBrackets);
-  console.log("closeBrackets- " + closeBrackets);
-  console.log("sameBrackets- " + sameBrackets);
-
+// Changing closing bracket if the brackets  same.
   let k = 0;
   for (let i = 0; i < sameBrackets.length; i++) {
     for (let j = 0; j < arrOfStr.length; j++) {
       if (arrOfStr[j] === sameBrackets[i]) {
-        console.log(arrOfStr[j] + "===" + sameBrackets[i]);
         k++;
-        console.log(k);
         if (!(k % 2)) {
           arrOfStr[j] = arrOfStr[j] + "!";
-          console.log(arrOfStr[j]);
           k = 0;
         }
       }
     }
   }
 
-
-
   for (let i = 0; i < arrOfStr.length; i++) {
-
-    console.log("Обработка " + arrOfStr[i]);
-
     if (openBrackets.indexOf(arrOfStr[i]) > -1) {
-        console.log(arrOfStr[i] + " in " + openBrackets);
-        bracketsStack.push(arrOfStr[i]);
-        console.log(arrOfStr[i] + "- go to stack");
+      bracketsStack.push(arrOfStr[i]);
       } else {
-
-            console.log(arrOfStr[i] + "- not open, then analize");
-            console.log("bracketsStack-" + bracketsStack);
-
-        if (bracketsStack.lastIndexOf(openBrackets[closeBrackets.indexOf(arrOfStr[i])]) > -1) {
-          bracketsStack.length = bracketsStack.lastIndexOf(openBrackets[closeBrackets.indexOf(arrOfStr[i])]);
+        let lastIndexOfSuitableOpeningBracket = bracketsStack.lastIndexOf(openBrackets[closeBrackets.indexOf(arrOfStr[i])]);
+        if (lastIndexOfSuitableOpeningBracket > -1) {
+          bracketsStack.length = lastIndexOfSuitableOpeningBracket;
         } else {
-            bracketsStack.push(arrOfStr[i]);
+          bracketsStack.push(arrOfStr[i]);
         }
-        console.log(bracketsStack);
       }
-    }
+  }
 
-  console.log("final");
-  console.log(bracketsStack);
   if (bracketsStack.length === 0) {
     return true;
   } else {
     return false;
   }
-  // your solution
 }
